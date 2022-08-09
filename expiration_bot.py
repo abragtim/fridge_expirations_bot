845a1b0665d557248d0f89ed1d06dd91a5e27075
Timur Abragimovich <timurkatime@gmail.com>
2022-08-09 02:29:16 +0200

    async updates + logger
    - infinity_polling
    - logger

--- expiration_bot.py
+++ expiration_bot.py
@@ -1,7 +1,7 @@
 import asyncio
 import datetime
 from telebot.async_telebot import AsyncTeleBot
-from logs import my_logger
+from logger import logger
 
 
 def bot_server(token):
@@ -27,17 +27,19 @@ def bot_server(token):
             await bot.reply_to(message, 'Usage: <product> <expiration date>')
 
 
+
     async def expiration_timer(product: str, date: str, user):
         ''' Seting an expiration timer by user for product on current date. '''
-        my_logger.logger(("user's request", user, product, date))
+        logger.info(f"User {user} request set {date} for {product}.")
+
         date = date.split('.')  # str -> list
         if len(date) < 3:
             # need at leatst 3 args for setting
             await bot.send_message(
                 user,
                 'Usage: <product> <expiration date>. Format of date is D.M.Y')
-            my_logger.logger(
-                ('error', user, product, date, 'wrong date format'))
+            logger.error(f"wrong request: expiration date {date} for {product} by user = {user}")
+
         try:
             date = datetime.datetime(int(date[2]), int(date[1]), int(date[0]))
         except TypeError:
@@ -45,8 +47,8 @@ def bot_server(token):
             await bot.send_message(
                 user,
                 'Wrong year/month/day format. Correct: D.M.Y (exapmle: 3.1.2022)')
-            my_logger.logger('error', user, product, date,
-                             'Wrong year/month/day format')
+            logger.error(f'Wrong year/month/day format: user = {user}, product = {product}, date = {date}')
+
         date_delta = date - datetime.datetime.today()  # days before expire
         # datetime -> int (seconds)
         timer = date_delta.days * 24 * 60 + date_delta.seconds
@@ -55,4 +57,4 @@ def bot_server(token):
         await bot.send_message(user, f"Don't eat {product}!")  # beep!
 
 
-    asyncio.run(bot.polling())  # bot activating
+    asyncio.run(bot.infinity_polling())  # bot activating
